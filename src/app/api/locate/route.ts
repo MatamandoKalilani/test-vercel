8927efda19e38081fe0d3c5e41622d8b80bdfd57
Mata00616 <63013364+MatamandoKalilani@users.noreply.github.com>
2024-03-31 23:46:55 +0200

    Save

--- src/app/api/locate/route.ts
+++ src/app/api/locate/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const region = process.env.NOW_REGION || "Region not available";
 
